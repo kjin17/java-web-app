613700c15b40adb233b810696cd27df4e2420bb8
kjin17 <60906489+kjin17@users.noreply.github.com>
2023-02-03 15:28:17 +0900

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String str = "Greetings from Spring Boot + Version 3";
+		String str = "Greetings from Spring Boot + Version 4";
 		return str;
 	}
 
